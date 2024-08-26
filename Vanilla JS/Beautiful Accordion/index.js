a8643b72c6e4f17fa4b83b40122c166d28f97e72
HemantDutta <hemantnorddutta2@gmail.com>
2024-08-26 19:15:14 +0530

    Create Beautiful Accordion

---
+++ Vanilla JS/Beautiful Accordion/index.js
@@ -0,0 +1,12 @@
+document.querySelectorAll(".accordion-item").forEach(item => {
+    item.addEventListener("click", (e)=> accordionControls(e))
+})
+
+function accordionControls(e) {
+    let target = e.target;
+    let itemList = document.querySelectorAll(".accordion-item");
+
+    itemList.forEach(item => item !== target ? item.classList.remove("item-open"): null);
+
+    target.classList.add("item-open");
+}
