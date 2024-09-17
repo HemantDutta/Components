67b678d06aedfec41403cc2e05762d60aa3a0155
HemantDutta <hemantnorddutta2@gmail.com>
2024-09-17 23:17:46 +0530

    Complete Flip Card

---
+++ Vanilla JS/Smooth Flip Card/index.js
@@ -0,0 +1,29 @@
+const flipCards = document.querySelectorAll('.flip-card-wrapper');
+
+flipCards.forEach(card => {
+
+    let rotateYFrontValue = 0;
+    let rotateYBackValue = -180;
+
+    card.addEventListener('mouseenter', function() {
+        const front = this.querySelector('.flip-card-front');
+        const back = this.querySelector('.flip-card-back');
+
+        rotateYFrontValue += 180;
+        rotateYBackValue += 180;
+
+        front.style.transform = `rotateY(${rotateYFrontValue}deg)`;
+        back.style.transform = `rotateY(${rotateYBackValue}deg)`;
+    });
+
+    card.addEventListener('mouseleave', function() {
+        const front = this.querySelector('.flip-card-front');
+        const back = this.querySelector('.flip-card-back');
+
+        rotateYFrontValue += 180;
+        rotateYBackValue += 180;
+
+        front.style.transform = `rotateY(${rotateYFrontValue}deg)`;
+        back.style.transform = `rotateY(${rotateYBackValue}deg)`;
+    });
+});
