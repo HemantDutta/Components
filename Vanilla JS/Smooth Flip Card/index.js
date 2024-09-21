61baf99e03947df210813690f2a9e660a0a57da0
HemantDutta <hemantnorddutta2@gmail.com>
2024-09-21 09:16:35 +0530

    Create Animated Masonry Grid

--- Vanilla JS/Smooth Flip Card/index.js
+++ Vanilla JS/Smooth Flip Card/index.js
@@ -1,13 +1,13 @@
-const flipCards = document.querySelectorAll('.flip-card-wrapper');
+const flipCards = document.querySelectorAll(".flip-card-wrapper");
 
 flipCards.forEach(card => {
 
     let rotateYFrontValue = 0;
     let rotateYBackValue = -180;
 
-    card.addEventListener('mouseenter', function() {
-        const front = this.querySelector('.flip-card-front');
-        const back = this.querySelector('.flip-card-back');
+    card.addEventListener("mouseenter", function () {
+        const front = this.querySelector(".flip-card-front");
+        const back = this.querySelector(".flip-card-back");
 
         rotateYFrontValue += 180;
         rotateYBackValue += 180;
@@ -16,14 +16,14 @@ flipCards.forEach(card => {
         back.style.transform = `rotateY(${rotateYBackValue}deg)`;
     });
 
-    card.addEventListener('mouseleave', function() {
-        const front = this.querySelector('.flip-card-front');
-        const back = this.querySelector('.flip-card-back');
+    card.addEventListener("mouseleave", function () {
+        const front = this.querySelector(".flip-card-front");
+        const back = this.querySelector(".flip-card-back");
 
         rotateYFrontValue += 180;
         rotateYBackValue += 180;
 
         front.style.transform = `rotateY(${rotateYFrontValue}deg)`;
         back.style.transform = `rotateY(${rotateYBackValue}deg)`;
-    });
-});
+    })
+})
