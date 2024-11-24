a6ca12d62812dd4a01e20a9f4f6489780a32b6d4
HemantDutta <hemantnorddutta2@gmail.com>
2024-11-24 11:11:54 +0530

    Analog Clock Final

--- Vanilla JS/Sexy Analog Clock/index.js
+++ Vanilla JS/Sexy Analog Clock/index.js
@@ -20,3 +20,25 @@ function displayTime(){
 }
 
 setInterval(displayTime,100);
+
+let date0 = document.getElementById('date-0');
+let date1 = document.getElementById('date-1');
+let daySpan = document.getElementById('day');
+
+const weekDays = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+function setDayDate() {
+    let date = new Date();
+    let dt = date.getDate();
+    let paddedDate = dt.toString().padStart(2, '0');
+
+    date0.innerText = paddedDate[0];
+    date1.innerText = paddedDate[1];
+
+    let day = date.getDay();
+    daySpan.innerText = weekDays[day];
+}
+
+setDayDate();
+
+setInterval(setDayDate, 60000);
